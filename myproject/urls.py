f2d15eee8281d2902fb593d6f6c7b49fbc53e852
Timmy <zhansj39@gmail.com>
2025-04-13 04:35:37 +0800

    fix: CSRF verification failed

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),  # 加入這一行
 ]
+
+# 僅在生產環境添加靜態文件路由（DEBUG=False 時）
+if not settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
